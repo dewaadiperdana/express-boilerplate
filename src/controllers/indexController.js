7a293c1468d739eb191ca245f455b013932212a6
Adi <dewaadiperdana@gmail.com>
2019-11-12 20:21:06 +0800

    Update for 1.1.0

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,7 +1,7 @@
 import ExampleService from '@services/exampleService';
 
 export default class IndexController {
-    static home(req, res) {
-        res.send('Express boilerplate is ready');
-    }
+  static home(req, res) {
+    res.send('Express boilerplate is ready');
+  }
 }
